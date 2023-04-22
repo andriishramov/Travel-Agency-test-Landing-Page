68e9706374e93ff6e76c4cc562c63d381410adbf
Andrii <shramov12@gmail.com>
2023-04-22 16:29:56 +0200

    few updates

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ function showMap(button) {
 
     // toggle the display of the iframe
     iframe.style.display = iframe.style.display === 'none' ? 'block' : 'none';
+
+    document.addEventListener('click', function(e) {
+        if (!iframe.contains(e.target) && e.target !== button) {
+            iframe.style.display = 'none';
+        }
+    });
+}
+function getOffer(button) {
+    let form = button.nextElementSibling;
+
+    // toggle the display of the iframe
+    form.style.display = form.style.display === 'none' ? 'block' : 'none';
+
+    document.addEventListener('click', function(e) {
+        if (!form.contains(e.target) && e.target !== button) {
+            form.style.display = 'none';
+        }
+    });
 }
 /*The expression iframe.style.display === 'none' ? 'block' : 'none' is called a ternary operator. It is a shorthand way of writing an if-else statement. It evaluates the condition iframe.style.display === 'none', which checks whether the CSS display property of the iframe is set to 'none'. If the condition is true, it returns the value 'block', indicating that the iframe should be displayed. If the condition is false, it returns the value 'none', indicating that the iframe should be hidden.*/
 
